fc6e0a17409c0a6af4386a56ba13901ff7168e3a
Jonny <jsfis612@dvusd.org>
2022-03-22 13:08:48 -0700

    Im gonna cry bro
    wtf

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -31,7 +31,7 @@ Partial Class Form1
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(85, 25)
+        Me.Button1.Location = New System.Drawing.Point(183, 117)
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(75, 23)
         Me.Button1.TabIndex = 0
